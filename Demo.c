68083bd05da44fd533d483191c819ec045897971
Mahesh Sanjayrao Bhosle <85066485+MSBHOSLE@users.noreply.github.com>
2022-12-27 19:22:39 +0530

    Add files via upload

---
+++ Demo.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("Jay Ganesh...");
+
+    return 0;
+}
---
+++ Ganesh.java
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+
+                            class    Ganesh
+                            {
+
+                                public  static  void    main    (String     arg[])
+                                {
+
+                                        System. out.    println("Jay Ganesh...");
+                                
+                                }
+                            }
